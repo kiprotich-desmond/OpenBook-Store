eb107d9f6af41ea2e9c8372d1154bc9dcfbd6731
kiprotich-desmond <desmondkiprotich37@gmail.com>
2026-08-27 20:52:59 +0000

    Replace design mockup with multi-page version

---
+++ design-mockup/tw-config.js
@@ -0,0 +1,4 @@
+tailwind.config = { theme: { extend: {
+  colors: { cream:'#F4EFE4', creamdim:'#ECE5D5', ink:'#221D18', inksoft:'#5C5348', rust:'#A8431C', rustdeep:'#7E3113', sand:'#D6C9AE', gold:'#E8A93B' },
+  fontFamily: { serif:['Fraunces','serif'], sans:['Public Sans','sans-serif'], mono:['IBM Plex Mono','monospace'] }
+} } };
